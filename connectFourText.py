3ee434582d2162dd4810e0a86203670c41d2ea87
S-Fumoffu <160081528+S-Fumoffu@users.noreply.github.com>
2025-04-28 00:17:17 -0700

    Update

--- connectFourText.py
+++ connectFourText.py
@@ -8,6 +8,26 @@ class ConnectFourPygame(TicTacToe):
 
     def __init__(self, h=7, v=6, k=4):
         TicTacToe.__init__(self, h, v, k)
+        
+        # Intializing Unique Features
+        self.game_states = {    0: "MODE_SELECT",
+                                1: "DIFFICULTY_SELECT",
+                                2: "PLAYING",
+        }
+
+        self.game_modes = {1: "Player vs Player",
+                           2: "Player vs AI"}
+        
+        self.ai_difficulties = {0: "N/A",
+                                1: "Easy",
+                                2: "Medium",
+                                3: "Hard"}
+        self.game_index = 0
+        self.mode_index = 0
+        self.difficulty_index = 0
+
+        # Players
+        self.player1 = self.player2 = human_player
 
     def actions(self, state):
         return [(x, y) for (x, y) in state.moves
@@ -37,6 +57,74 @@ class ConnectFourPygame(TicTacToe):
                 if self.terminal_test(state):
                     print(state)
                     return self.utility(state, self.to_move(self.initial))
+
+    def start_game(self):
+        self.input_mode()
+        self.initialize_mode()
+        utility = self.play_game(self.player1, self.player2)
+        
+        if (utility < 0):
+            print("Player 2 won the game")
+        else:
+            print("Player 1 won the game")
+
+    # Input Settings
+    def input_mode(self):
+        is_input_valid = False
+        input_mode = 0
+        input_difficulty = 0
+
+        # Display Prompt
+        print("Select Game Mode:" \
+            "\n 1. Player vs Player" \
+            "\n 2. Player vs AI")
+
+        # While the input is not valid:
+        while not is_input_valid:
+            input_mode = int(input('Input game mode number: '))
+            if input_mode in self.game_modes.keys():
+                is_input_valid = True
+                
+                # If selecting AI,
+                if input_mode == 2:
+                    # Display Prompt
+                    print("Select AI Difficulty:" \
+                        "\n 1. Easy" \
+                        "\n 2. Medium" \
+                        "\n 3. Hard")
+                    
+                    # Reset Input Validity
+                    is_input_valid = False
+                    
+                    # Break once a valid input is set
+                    while not is_input_valid:
+                        input_difficulty = int(input('Input difficulty number: '))
+                        if input_difficulty in self.ai_difficulties.keys() and not input_difficulty == 0: is_input_valid = True
+                        else:
+                            print("Invalid Input. Input 1 for easy, 2 for medium, 3 for hard.")
+            
+            else:
+                print("Invalid Input. Input 1 for PvP or 2 for PvAI")
+
+            self.mode_index = input_mode
+            self.difficulty_index = input_difficulty
+
+    # Initialize Game
+    def initialize_mode(self):
+        print("Game Mode: ", self.game_modes[self.mode_index])
+        print ("AI Difficulty: ", self.ai_difficulties[self.difficulty_index])
+        
+        if self.game_modes[self.mode_index] == "Player vs Player":
+            self.player1 = human_player
+            self.player2 = human_player
+        elif self.game_modes[self.mode_index] == "Player vs AI":
+            self.player1 = human_player
+            if self.ai_difficulties[self.difficulty_index] == "Easy":
+                self.player2 = ai_player_easy
+            elif self.ai_difficulties[self.difficulty_index] == "Medium":
+                self.player2 = ai_player_medium
+            elif self.ai_difficulties[self.difficulty_index] == "Hard":
+                self.player2 = ai_player_hard
 
 # Query Player
 def human_player(game, state):
@@ -69,79 +157,18 @@ def ai_player_medium(game, state):
 def ai_player_hard(game, state):
     return alpha_beta_cutoff_search(state, game, 8, None, None)
 
-# Initialize Game
-
-def initialize_mode():
-    is_input_valid = False
-    input_mode = 0
-    input_difficulty = 0
-    
-    game_modes = {1: "Player vs Player",
-                  2: "Player vs AI"}
-    
-    ai_difficulties = {0: "N/A",
-                       1: "Easy",
-                       2: "Medium",
-                       3: "Hard"}
-
-    # Display Prompt
-    print("Select Game Mode:" \
-        "\n 1. Player vs Player" \
-        "\n 2. Player vs AI")
-
-    # While the input is not valid:
-    while not is_input_valid:
-        input_mode = int(input('Input game mode number: '))
-        if input_mode in game_modes.keys():
-            is_input_valid = True
-            
-            # If selecting AI,
-            if input_mode == 2:
-                # Display Prompt
-                print("Select AI Difficulty:" \
-                      "\n 1. Easy" \
-                      "\n 2. Medium" \
-                      "\n 3. Hard")
-                
-                # Reset Input Validity
-                is_input_valid = False
-                
-                # Break once a valid input is set
-                while not is_input_valid:
-                    input_difficulty = int(input('Input difficulty number: '))
-                    if input_difficulty in ai_difficulties.keys() and not input_difficulty == 0: is_input_valid = True
-                    else:
-                        print("Invalid Input. Input 1 for easy, 2 for medium, 3 for hard.")
-        
-        else:
-            print("Invalid Input. Input 1 for PvP or 2 for PvAI")
-
-    print("Game Mode: ", game_modes[input_mode])
-    print ("AI Difficulty: ", ai_difficulties[input_difficulty])
-
-
-    if game_modes[input_mode] == "Player vs Player":
-        player1 = human_player
-        player2 = human_player
-    elif game_modes[input_mode] == "Player vs AI":
-        player1 = human_player
-        if ai_difficulties[input_difficulty] == "Easy":
-            player2 = ai_player_easy
-        elif ai_difficulties[input_difficulty] == "Medium":
-            player2 = ai_player_medium
-        elif ai_difficulties[input_difficulty] == "Hard":
-            player2 = ai_player_hard
-
-    return player1, player2
-
 if __name__ == "__main__":
     connectFour = ConnectFourPygame()
-
-    player1, player2 = initialize_mode()
-
-    utility = connectFour.play_game(player1, player2)
-
+    
+    connectFour.input_mode()
+    connectFour.initialize_mode()
+    
+    utility = connectFour.play_game(connectFour.player1, connectFour.player2)
+    
     if (utility < 0):
         print("Player 2 won the game")
     else:
         print("Player 1 won the game")
+
+
+    
